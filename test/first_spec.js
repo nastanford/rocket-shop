0087d47862e2a9ec43ca9f1a7a084bcb891c0899
Nathan Stanford Sr <nathan.stanford@gmail.com>
2017-09-27 09:33:36 -0400

    Initial Load

---
+++ test/first_spec.js
@@ -0,0 +1,10 @@
+// BDD Testing
+var assert = require("assert");
+
+describe("a feature", function(){
+    describe("A scenerio",function() {
+        it("does something expected", function(){
+            assert(true);
+        });
+    });
+});    
